9523abe09ea3adaa51123a94ddde7b25d29dc9f1
japhethlumanog <japhethlumanog3@gmail.com>
2026-01-07 22:43:42 +0800

    Finished week1 exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string playAgain;
+
+        do
+        {
+            
+        
+        
+            // Console.Write("What is magic number? ");
+            // string valueGiven = Console.ReadLine();
+            // int magicNumber = int.Parse(valueGiven);
+
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int guess = -1;
+
+            int numberGuesses = 0;
+
+
+            while (guess != magicNumber)
+            {
+                
+                Console.Write("What is your guess? ");
+                string guessGiven = Console.ReadLine();
+                guess = int.Parse(guessGiven);
+
+                numberGuesses += 1;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher!");
+                }
+
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower!");
+                }
+
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"It took you {numberGuesses} guesses.");
+                    break;
+                }
+
+            } // end guessing loop
+
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+        } while (playAgain != null && playAgain.Equals("yes", StringComparison.OrdinalIgnoreCase));
+        // end game loop
+        
+
+        
+
     }
 }
