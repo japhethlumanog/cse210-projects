b97d12be1e87cce1b59697333ae0a84e3ee5bb16
japhethlumanog <japhethlumanog3@gmail.com>
2026-01-07 18:11:04 +0800

    Finished the week1 exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,80 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage: ");
+        string valueInText = Console.ReadLine(); 
+        int number = int.Parse(valueInText);
+
+        string letter = string.Empty;
+
+        if (number >= 90 )
+        {
+            letter = "A";
+        }
+
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+        // adding sign
+        string sign = string.Empty;
+        float lastDegit = number % 10;
+
+        if (lastDegit >= 7)
+        {
+            sign = "+";
+        }
+
+        else if (lastDegit <= 3)
+        {
+            sign = "-";
+        }
+
+        else
+        {
+            sign = "";
+        }
+
+        // Handle the A+ grades
+
+        if (number >= 93)
+        {
+            sign = "";
+        }
+
+        // Handle the F+ and F- grades
+
+        if (letter == "F")
+        {
+            sign = "";
+        }
+        Console.WriteLine($"Your grade is {sign}{letter}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulation you pass the course.");
+        }
+        
+        else
+        {
+            Console.WriteLine("Unfortunately you did not pass the course. We encourage you to take the course again.");
+            
+        }
+
     }
 }
